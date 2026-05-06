149807de400ac0ea5c8e2d5d1ce69353c4037132
cc24146 <166397026+cc24146@users.noreply.github.com>
2026-05-06 12:26:52 -0300

    mais um exercicio

--- exerciciosclaude/ec6.c
+++ exerciciosclaude/ec6.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void alocar_vetor(int tamanho, int *vetor);
 int main(){
     int tamanho_do_vetor;
     scanf("%d", &tamanho_do_vetor);
     int *vet;
-    alocar_vetor(tamanho_do_vetor, vet);
-    for(int i = 0; i < tamanho_do_vetor; i++){
-        printf("%d", vet[i]);
+
+    vet = (int *)malloc(tamanho_do_vetor* sizeof(int));
+    for(int i = 0; i < tamanho_do_vetor; i ++){
+        vet[i] = i*i;
+        printf("%d\t", vet[i]);
     }
+    printf("\n");
     free(vet);
     return 0;
-}
-
-void alocar_vetor(int tamanho, int *vetor){
-    vetor = (int *)malloc(tamanho* sizeof(int));
-    for(int i = 0; i < tamanho; i ++){
-        vetor[i] = i*i;
-    }
-
 }
